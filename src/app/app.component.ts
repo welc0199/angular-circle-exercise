f698995d7d69620303ad2732d66f403afa24f56f
Erin Welch <erwelch@microsoft.com>
2018-02-06 12:19:06 -0800

    Working clicks.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,61 +14,7 @@ export interface Circle {
 })
 export class AppComponent {
   public circlesStarted: boolean = false;
-
-  public circles: Circle[] = [
-    {
-      id: 1,
-      color: '#DABFFF',
-      top: `${this.randomizeTop()}%`,
-    },
-    {
-      id: 2,
-      color: '#907AD6',
-      top: `${this.randomizeTop()}%`,
-    },
-    {
-      id: 3,
-      color: '#4F518C',
-      top: `${this.randomizeTop()}%`,
-    },
-    {
-      id: 4,
-      color: '#2C2A4A',
-      top: `${this.randomizeTop()}%`,
-    },
-    {
-      id: 5,
-      color: '#7FDEFF',
-      top: `${this.randomizeTop()}%`,
-    },
-    {
-      id: 6,
-      color: '#9B5094',
-      top: `${this.randomizeTop()}%`,
-      
-    },
-    {
-      id: 7,
-      color: '#8FB8DE',
-      top: `${this.randomizeTop()}%`,
-    },
-    {
-      id: 8,
-      color: '#253237',
-      top: `${this.randomizeTop()}%`,
-    },
-    {
-      id: 9,
-      color: '#59C3C3',
-      top: `${this.randomizeTop()}%`,
-    },
-    {
-      id: 10,
-      color: '#F45B69',
-      top: `${this.randomizeTop()}%`,
-    },
-  ]
-
+  public showSameColor: boolean = false;
 
     public showCircles(): void {
       this.circlesStarted = true;
@@ -82,7 +28,60 @@ export class AppComponent {
       this.circles[circleId - 1].showId = true;
       setTimeout(() => {
           this.circles[circleId - 1].showId = false;
-          console.log('WHAT THE HELL?!')
-      }, 3000);
+      }, 4000);
     }
+
+    public circles: Circle[] = [
+      {
+        id: 1,
+        color: '#DABFFF',
+        top: `${this.randomizeTop()}%`,
+      },
+      {
+        id: 2,
+        color: '#907AD6',
+        top: `${this.randomizeTop()}%`,
+      },
+      {
+        id: 3,
+        color: '#4F518C',
+        top: `${this.randomizeTop()}%`,
+      },
+      {
+        id: 4,
+        color: '#2C2A4A',
+        top: `${this.randomizeTop()}%`,
+      },
+      {
+        id: 5,
+        color: '#7FDEFF',
+        top: `${this.randomizeTop()}%`,
+      },
+      {
+        id: 6,
+        color: '#9B5094',
+        top: `${this.randomizeTop()}%`,
+        
+      },
+      {
+        id: 7,
+        color: '#8FB8DE',
+        top: `${this.randomizeTop()}%`,
+      },
+      {
+        id: 8,
+        color: '#253237',
+        top: `${this.randomizeTop()}%`,
+      },
+      {
+        id: 9,
+        color: '#59C3C3',
+        top: `${this.randomizeTop()}%`,
+      },
+      {
+        id: 10,
+        color: '#F45B69',
+        top: `${this.randomizeTop()}%`,
+      },
+    ]
 }
